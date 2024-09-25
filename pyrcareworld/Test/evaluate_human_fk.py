4b01149b11c8367365d6a8cb4c3e28251ae75973
thisjustin123 <jjg283@cornell.edu>
2024-09-24 23:59:28 -0400

    Add missing cloth scripts

--- pyrcareworld/Test/evaluate_human_fk.py
+++ pyrcareworld/Test/evaluate_human_fk.py
@@ -10,11 +10,21 @@ if __name__ == "__main__":
     env = RhiVisEnv(
         executable_file="@editor",
     )
-    for _ in range(50):
-        shoulder_x = random.random() * 90 + 90
-        shoulder_y = random.random() * 90
-        shoulder_z = random.random() * 90
-        elbow = random.random() * 90
+
+    shoulder_angles = np.load("pyrcareworld/Test/shoulder_elbow_angles.npy")
+    # Scramble
+    shoulder_angles = np.random.permutation(shoulder_angles)
+    # Take every Nth
+    N = 50
+    shoulder_angles = shoulder_angles[::N]
+
+    for rot_vector in shoulder_angles:
+        shoulder_x = rot_vector[0]
+        shoulder_y = 180 - rot_vector[2]
+        shoulder_z = rot_vector[1]
+        elbow = rot_vector[3]
+
+        print(shoulder_x, shoulder_y, shoulder_z, elbow)
 
         env.fk_step(
             shoulder_angles=[shoulder_x, shoulder_y, shoulder_z],
@@ -26,6 +36,7 @@ if __name__ == "__main__":
     # Read pyrcareworld/Test/evaluate_human_fk.npy
     as_np = np.load("pyrcareworld/Test/evaluate_human_fk.npy")
     # Demo: Can read points afterwards.
+    env.cloud_manager.set_radius(0.3)
     env.cloud_manager.make_cloud(points=as_np, name="Human FK")
 
     for _ in range(300):
---
+++ pyrcareworld/Test/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+angles = np.load("pyrcareworld/Test/shoulder_elbow_angles.npy")
+
+print(angles.shape)
